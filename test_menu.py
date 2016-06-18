a1a6c113ea52657d9c746d680fac604242b3d7bf
Aswa Paul <aswapaul@gmail.com>
2016-06-18 16:23:17 +0300

    Add test food instance

--- test_menu.py
+++ test_menu.py
@@ -5,24 +5,30 @@
 import unittest
 import controller
 import menu
-
+import food
 
 class  Test_menuTestCase(unittest.TestCase):
     #def setUp(self):
     #    new_menu = self.menu.Menu("Foods")
     
     def setUp(self):
-        self._addition = menu.Menu("Foood")
+        food_name = "Food_name"
+        price = 20
+        value = "Carbs"
+        pref = ""
+        food_list = [food_name,price,value,pref]
+        self._addition = menu.Menu(food_list)
+        self._addition2 = food.Food(food_name,price,value,pref)
         
     def test_menu_instance(self):
         self.assertIsInstance(self._addition, menu.Menu)
         
-
+    def test_food_instance(self):
+        self.assertIsInstance(self._addition2, food.Food)
+    
     #def tearDown(self):
     #    self.foo.dispose()
     #    self.foo = None
 
-
 if __name__ == '__main__':
     unittest.main()
-
