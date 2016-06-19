f77bfd43966dab36299e23e16be092649f877b7f
Aswa Paul <aswapaul@gmail.com>
2016-06-19 21:15:01 +0300

    Add new tests

--- test_menu.py
+++ test_menu.py
@@ -8,22 +8,36 @@ import menu
 import food
 
 class  Test_menuTestCase(unittest.TestCase):
-    #def setUp(self):
-    #    new_menu = self.menu.Menu("Foods")
     
     def setUp(self):
-        food_name = "Food_name"
-        price = 20
-        value = "Carbs"
-        pref = ""
-        food_list = [food_name,price,value,pref]
-        self._addition = menu.Menu(food_list)
-        self._addition2 = food.Food(food_name,price,value,pref)
+        """Sets global variables"""
         
+        self.food_name = "Food_name"
+        self.price = 20
+        self.value = "Carbs"
+        self.pref = ""
+        self.food_list = [self.food_name,self.price,self.value,self.pref]
+        self._addition = menu.Menu(self.food_list)
+        self._addition2 = food.Food(self.food_name,self.price,self.value,self.pref)
+    
+    def test_menu_object(self):
+        """Tests if type of menu.Menu"""
+        
+        self.assertEqual(True, type(self._addition) is menu.Menu)
+    
+    def test_food_object(self):
+        """Tests if type of food.Food"""
+        
+        self.assertEqual(True, type(self._addition2) is food.Food)
+    
     def test_menu_instance(self):
+        """Tests if is menu.Menu instance"""
+        
         self.assertIsInstance(self._addition, menu.Menu)
         
     def test_food_instance(self):
+        """Tests if is food.Food instance"""
+        
         self.assertIsInstance(self._addition2, food.Food)
     
 if __name__ == '__main__':
